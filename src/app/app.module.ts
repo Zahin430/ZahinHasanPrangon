bec585ba8a5c155d2301fba5190b2fd598f5f5e6
Zahin430 <zhprangon@mun.ca>
2019-03-07 01:20:18 -0330

    Added features for the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { AppComponent } from './app.component';
+
+import { ParticlesModule } from 'angular-particle';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ParticlesComponent } from './particles/particles.component';
+import { PanelComponent } from './panel/panel.component';
+import { HeaderAnimationComponent } from './header-animation/header-animation.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParticlesComponent,
+    PanelComponent,
+    HeaderAnimationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ParticlesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header-animation/header-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-animation',
+  templateUrl: './header-animation.component.html',
+  styleUrls: ['./header-animation.component.css']
+})
+export class HeaderAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
